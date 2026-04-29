c44540b02558b6470dbb0497e2e62f00ab35b2a0
MSabbirHossen <mshossen724@gmail.com>
2026-04-29 09:50:06 +0300

    data is visble in the server.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const phones = require('./phones.json');
-const app = express()
-const port = 3000
+const express = require("express");
+const phones = require("./phones.json");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!, Server is running on port 3000 with nodemon')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!, Server is running on port 3000 with nodemon");
+});
 
-app.get('/phones', (req, res) => {
-    res.send(phones)
-})
- app.get('/phones/:id', (req, res) => {
-    const id = req.params.id;
-    const phone = phones.find(p => p.id === id);
-    if (phone) {
-        res.send(phone);
-    } else {
-        res.status(404).send('Phone not found');
-    }
+app.get("/phones", (req, res) => {
+  res.send(phones);
+});
+app.get("/phones/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const phone = phones.find((p) => p.id === id);
+  if (phone) {
+    res.send(phone);
+  } else {
+    res.status(404).send("Phone not found");
+  }
 });
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
